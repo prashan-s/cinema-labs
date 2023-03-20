af3d940882343e79d004ab84c27f17880e7addf4
Prashan Samarathunge <prashansamarathunge@gmail.com>
2023-03-20 18:57:03 +0530

    Add 404 error page template

---
+++ templates/404.php
@@ -0,0 +1,20 @@
+<?php
+// templates/404.php
+?>
+
+<div class="error-page">
+    <div class="error-content">
+        <h1 class="error-title">404</h1>
+        <h2 class="error-subtitle">Content Not Found</h2>
+        <p class="error-description">
+            Sorry, we couldn't find the content you're looking for. 
+            It might have been removed or the link is incorrect.
+        </p>
+        <a href="/" class="btn btn-primary">
+            <svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 -960 960 960" width="20" fill="white">
+                <path d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z"/>
+            </svg>
+            Back to Home
+        </a>
+    </div>
+</div>
